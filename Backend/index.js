41a114ae41bd90432e7986d81e65e55fbc0d80e0
JonSEK <engkien@ymail.com>
2024-03-06 18:17:53 +0800

    Update index.js file to include path module

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,12 @@ import connectDB from "./config/db.js";
 import fruitRouter from "./routes/fruits.js";
 import purchaseRouter from "./routes/purchase.js";
 import cors from "cors";
-import path from "path";
 
 const port = process.env.PORT;
 
 const app = express();
+
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 app.use(cors());
