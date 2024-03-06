3ff2f301e7bc600759b52d7092164109be4404de
JonSEK <engkien@ymail.com>
2024-03-06 18:13:59 +0800

    Add static file serving for frontend

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,13 @@ import connectDB from "./config/db.js";
 import fruitRouter from "./routes/fruits.js";
 import purchaseRouter from "./routes/purchase.js";
 import cors from "cors";
+import path from "path";
 
 const port = process.env.PORT;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../Frontend/dist')));
 app.use(cors());
 app.use(express.json());
 app.use("/api", fruitRouter);
